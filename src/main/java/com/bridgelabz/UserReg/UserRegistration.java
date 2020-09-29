8a1fed9c92f08050f2bed81d9c70f7207673fb3e
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-29 16:36:31 +0530

    [Prerna]ADDED: First Name validation and test cases

--- src/main/java/com/bridgelabz/UserReg/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserReg/UserRegistration.java
@@ -1,10 +1,17 @@
 package com.bridgelabz.UserReg;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	
 	public void welcomeMessage()
 	{
 		System.out.println("Welcome to User Registration!");
 	}
+	
+	public boolean checkFirstName(String s) {
+		Pattern FNAME_PATTERN = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+        return FNAME_PATTERN.matcher(s).matches();
+    }
 
 }
--- src/test/java/com/bridgelabz/UserReg/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserReg/UserRegistrationTest.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.UserReg;
 
-import static org.junit.Assert.assertTrue;
+//import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,9 +12,34 @@ public class UserRegistrationTest
     UserRegistration userReg = null;
     
     @Before
-    void intialize()
+    public void intialize()
     {
     	userReg = new UserRegistration();
     }
+    
+    @Test
+    public void givenName_WhenPatternMatches_ReturnTrue()
+    {
+    	String name = "Prerna";
+    	boolean fnameValid = userReg.checkFirstName(name);
+    	Assert.assertEquals(true,fnameValid);
+    }
+    @Test
+    public void givenName_WhenLessThanThreeCharacters_ReturnFalse()
+    {
+    	String name = "Pr";
+    	boolean fnameValid = userReg.checkFirstName(name);
+    	Assert.assertEquals(false,fnameValid);
+    }
+    @Test
+    public void givenName_WhenDoesNotStartWithCapital_ReturnFalse()
+    {
+    	String name = "prerna";
+    	boolean fnameValid = userReg.checkFirstName(name);
+    	Assert.assertEquals(false,fnameValid);
+    }
+    
+
+	
    
 }
