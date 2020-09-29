987505351f4c06d6aff891fd7b090142bc2e2aa8
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-29 16:24:23 +0530

    [Prerna]ADDED: Welcome mesage

---
+++ src/main/java/com/bridgelabz/UserReg/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.UserReg;
+
+public class UserRegistration {
+	
+	public void welcomeMessage()
+	{
+		System.out.println("Welcome to User Registration!");
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/UserReg/UserRegistrationTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.UserReg;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class UserRegistrationTest 
+{
+    UserRegistration userReg = null;
+    
+    @Before
+    void intialize()
+    {
+    	userReg = new UserRegistration();
+    }
+   
+}
